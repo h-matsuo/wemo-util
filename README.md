ef6409c03125d723251717957d936b62a4993b33
Hiroyuki Matsuo <hiroyuki3569825@gmail.com>
2016-09-24 16:30:19 +0900

    issue1 に対応
    * ログに現在時刻を追加
    * ログの外部出力機能を追加
    - node wemo-util --log output.log track --restheart http://localhost:8080/xx/yy/
    
    # ついでの修正
    * トラック中に WeMo へのリクエストが失敗した場合，以降 RESTHeart に書き込まなくなっていた問題を修正
    * LAN 内の WeMo の検索時，タイムアウト時間を 1 秒に変更
    
    # 個人的な好みの問題での修正
    * 電力消費のログファイルのファイル名を log.txt から track.log に変更
    * 日付のフォーマットを YYYY/MM/DD HH/MM/SS.mmm から YYYY/MM/DD-HH/MM/SS.mmm に変更

--- README.md
+++ README.md
@@ -13,11 +13,16 @@ WeMo Insight Switch Utility Tool by Hiroyuki Matsuo
 ## Usage
 
 ```
-Usage: node wemo-util <command> [<args>]
+Usage: node [--log <path>] wemo-util <command> [<args>]
 
 wemo-util は，Node.js から同一ローカルネットワーク内の WeMo Insight Switch を
 操作することができるユーティリティツールです．
 Node v4.4.4 での動作を確認しています．
+
+次のオプションが使用できます：
+
+--log <path>
+        ログを <path> で示す外部ファイルに出力します．
 
 次のコマンドが使用できます：
 
@@ -33,5 +38,5 @@ track [--restheart <URL>]
         電力消費状況などを，約 1 秒ごとに監視します．
         ログデータは，デフォルトではカレントディレクトリに書き出します．
         --restheart を指定すると，<URL> の RESTHeart にログデータを送信します．
-        <URL> = http://<host>:<port>/<data_base>/<collection>/
+        <URL> := http://<host>:<port>/<data_base>/<collection>/
 ```
--- wemo-util.js
+++ wemo-util.js
@@ -5,6 +5,9 @@ var fs      = require('fs');
 var ajax    = require('superagent');
 var search  = require('./lib/search.js');
 var request = require('./lib/request.js');
+var convert_date = require('./lib/convert_date.js');
+
+var log_out = null;
 
 // エントリポイント
 if (require.main === module) {
@@ -16,6 +19,12 @@ function main() {
     // コマンドライン引数を取得
     var argv = process.argv;
     argv.shift(); argv.shift(); // 先頭の node wemo-util は削除
+
+    // ログファイルの出力先を取得
+    if (argv[0] === '--log') {
+        log_out = argv[1];
+        argv.shift(); argv.shift();
+    }
 
     // コマンドが指定されていなければ Usage を表示
     if (argv.length < 1) {
@@ -48,18 +57,89 @@ function print_usage() {
 }
 
 function exec_search() {
-    console.log('Looking for WeMo Insight Switch...');
+    print_log('Looking for WeMo Insight Switch...');
     search(function (location) {
         if (location === null) {
-            console.log('No WeMo Insight Switches are found.');
+            print_log('No WeMo Insight Switches are found.');
         } else {
-            console.log('WeMo Insight Switch has found:');
-            console.log(location);
+            print_log('WeMo Insight Switch has found: ' + location.host + ':' + locatoin.port);
         }
     });
 }
 
-/*
+function exec_track(argv) {
+    print_log('Start tracking...');
+    console.log('"Ctrl + c" to quit.');
+    track_wemo(argv);
+}
+
+function track_wemo(argv) {
+    print_log('Looking for WeMo Insight Switch...');
+    var interval_id;
+    // WeMo が見つかるまで繰り返す
+    interval_id = setInterval(function () {
+        search(function (location) {
+            if (location === null) {
+                print_log('Error: No WeMo Insight Switches are found; I\'ll try again in 1 sec...');
+                return;
+            }
+            print_log('WeMo Insight Switch has found: ' + location.host + ':' + locatoin.port);
+            clearInterval(interval_id);
+            // WeMo へのリクエストが失敗するまで繰り返す
+            interval_id = setInterval(function () {
+                request(location, function (err, log_entry) {
+                    if (err) {
+                        clearInterval(interval_id);
+                        print_log('Error: Problem with request to WeMo: ' + err);
+                        track_wemo(argv);
+                        return;
+                    }
+                    // Ajax で RESTHeart に送信
+                    if (argv[0] === '--restheart') {
+                        ajax
+                            .post(argv[1])  // URL
+                            .set('Content-Type', 'application/json')
+                            .send(JSON.stringify(log_entry, null, '    '))
+                            .end(function(err, res){
+                                if (err) {
+                                    print_log('Error: Problem with insertion to MongoDB:\n' + JSON.stringify(err, null, '    '));
+                                    return;
+                                }
+                            });
+                    // track.log ファイルに追記
+                    } else {
+                        fs.appendFile('./track.log', JSON.stringify(log_entry, null, '    '), 'utf8', function (err) {
+                            if (err) {
+                                print_log('Error: Can\'t open the log file: track.log');
+                                return;
+                            }
+                        });
+                    }
+                });
+            }, 1000);
+        });
+    }, 1000);
+}
+
+function print_log(msg) {
+    var date = convert_date(new Date());
+    var log_data = '[' + date + '] ' + msg;
+    if (log_out === null) {
+        console.log(log_data);
+    } else {
+        log_data += '\n';
+        fs.appendFile(log_out, log_data, 'utf8', function (err) {
+            if (err) {
+                var file_path = log_out;
+                log_out = null;
+                console.log(log_data.substring(0, log_data.length - 1));
+                print_log('Error: Can\'t open the log file: ' + file_path);
+            }
+        });
+    }
+}
+
+/***** OLD SOURCE *****
 function exec_track(argv) {
     console.log('Start tracking...');
     console.log('"Ctrl + C" to quit.')
@@ -96,58 +176,3 @@ function exec_track(argv) {
     }, 1000);
 }
 */
-
-function exec_track(argv) {
-    console.log('Start tracking...');
-    console.log('"Ctrl + C" to quit.');
-    track(argv);
-}
-
-function track(argv) {
-    console.log('Looking for WeMo Insight Switch...');
-    var interval_id;
-    // WeMo が見つかるまで繰り返す
-    interval_id = setInterval(function () {
-        search(function (location) {
-            if (location === null) {
-                console.log('Error: No WeMo Insight Switches are found; I\'ll try again in 1 sec...');
-                return;
-            }
-            console.log('WeMo Insight Switch has found:');
-            console.log(location);
-            clearInterval(interval_id);
-            // WeMo へのリクエストが失敗するまで繰り返す
-            interval_id = setInterval(function () {
-                request(location, function (log_entry) {
-                    if (log_entry === null) {
-                        clearInterval(interval_id);
-                        track();
-                        return;
-                    }
-                    // Ajax で RESTHeart に送信
-                    if (argv[0] === '--restheart') {
-                        ajax
-                            .post(argv[1])  // URL
-                            .set('Content-Type', 'application/json')
-                            .send(JSON.stringify(log_entry, null, '    '))
-                            .end(function(err, res){
-                                if (err !== null) {
-                                    console.log('Error: Problem with inserting document to MongoDB:');
-                                    console.log(err);
-                                    return;
-                                }
-                            });
-                    // log.txt ファイルに追記
-                    } else {
-                        fs.appendFile('./log.txt', JSON.stringify(log_entry, null, '    '), 'utf8', function (err) {
-                            if (err) {
-                                console.log('Error: Can\'t append log data to the file.');
-                                return;
-                            }
-                        });
-                    }
-                });
-            }, 1000);
-        });
-    }, 1000);
-}
